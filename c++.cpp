8501bac3e6ff6a9f60a3cbb2fbe3b6c711068a8d
Mohitkant01 <168455303+Mohitkant01@users.noreply.github.com>
2025-04-20 17:54:48 +0530

    Add files via upload

---
+++ c++.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+int main() {
+    double num1, num2;
+    char op;
+
+    std::cout << "Enter first number: ";
+    std::cin >> num1;
+
+    std::cout << "Enter operator (+, -, *, /): ";
+    std::cin >> op;
+
+    std::cout << "Enter second number: ";
+    std::cin >> num2;
+
+    double result;
+
+    switch (op) {
+        case '+':
+            result = num1 + num2;
+            std::cout << "Result: " << result << std::endl;
+            break;
+        case '-':
+            result = num1 - num2;
+            std::cout << "Result: " << result << std::endl;
+            break;
+        case '*':
+            result = num1 * num2;
+            std::cout << "Result: " << result << std::endl;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                std::cout << "Result: " << result << std::endl;
+            } else {
+                std::cout << "Error: Division by zero is not allowed." << std::endl;
+            }
+            break;
+        default:
+            std::cout << "Invalid operator!" << std::endl;
+    }
+
+    return 0;
+}
